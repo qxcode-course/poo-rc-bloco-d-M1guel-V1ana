29008069a38592f1e52d4720d06b273fc4c1ff1f
Miguel Ângelo <vianamiguelangelo@gmail.com>
2025-11-26 13:35:34 +0000

    fazendo configs

--- tasks/poo/agenda/py/draft.py
+++ tasks/poo/agenda/py/draft.py
@@ -1,2 +1,19 @@
-class fone:
-    def __init__(self)
+class Fone:
+    def __init__(self, id: str, numero: int):
+        self.id = id 
+        self.numero = numero
+
+    def getId(self):
+        return self.id 
+    def getNumero(self):
+        return self.numero
+    
+    def __str__(self):
+        return f"{self.id}:{self.nome}"
+    
+
+
+class Contato:
+    def __init__(self, nome: str):
+        self.nome = nome 
+        self.fone = list[Fone] = []
