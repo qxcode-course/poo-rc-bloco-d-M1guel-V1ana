d446367e83d2cb73f02ebc3adb7651992ded1f9f
Miguel Ângelo <vianamiguelangelo@gmail.com>
2025-11-27 16:57:39 +0000

    iniciando do fone

--- tasks/poo/agenda/py/draft.py
+++ tasks/poo/agenda/py/draft.py
@@ -3,4 +3,14 @@ class Fone:
         self.__id = id 
         self.__number = number
     
-    def getId(self)
+    def getId(self):
+        return self.__id
+    def getNumber(self):
+        return self.__number
+    
+    def isValid(self) -> bool:
+        p = "123456789()."
+        return all(c in p for c in self.__number)
+    
+    def __str__(self):
+        return f"{self.__id} {self.__number}"
