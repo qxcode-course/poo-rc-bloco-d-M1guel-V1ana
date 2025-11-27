573b3111378a52b026b87fa9f960c3a299fbc3b9
Miguel Ângelo <vianamiguelangelo@gmail.com>
2025-11-27 16:50:10 +0000

    reiniciando a agenda

--- tasks/poo/agenda/py/draft.py
+++ tasks/poo/agenda/py/draft.py
@@ -1,21 +1,6 @@
 class Fone:
-    def __init__(self, id: str, numero: int):
-        self.id = id 
-        self.numero = numero
+    def __init__(self, id: str, number: str):
+        self.__id = id 
+        self.__number = number
     
-    def getId(self):
-        return self.id
-    def getNumero(self):
-        return self.numero
-    
-    def __str__(self):
-        return f"{self.id}:{self.nome}"
-    
-    def isValid(self) -> bool:
-        fone = Fone 
-        if not self.id or self.numero:
-            return False
-        if any(c.isalpha() for c in self.numero):
-            return False
-        return True
-class 
+    def getId(self)
